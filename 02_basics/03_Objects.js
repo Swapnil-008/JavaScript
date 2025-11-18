9e2c5500fd2266ca291a3d3cc6ed844b90bf257d
Swapnil <swapnilshingne155@gmail.com>
2025-11-19 00:09:24 +0530

    Fix typos and improve comments in 03_Objects.js; update symbol key value and clarify console log examples.

--- 02_basics/03_Objects.js
+++ 02_basics/03_Objects.js
@@ -5,17 +5,17 @@ const myObj = {
     name: "swapnil",
     "full name": "swapnil Shingne",
     email: "swapnilshingne155@gmail.com",
-    [mySym]: "key1",
+    [mySym]: "mykey1",
     location: "pune",
     married: false,
     languages: ["CPP", "JAVA"]
 }
 
 console.log(myObj.name)  // swapnil
-console.log(myObj[name]) // this will generate error
+// console.log(myObj[name]) // this will generate error
 console.log(myObj["name"]) // swapnil // This syntax is used for accessing entities like "full name", symbol
-console.log(myObj.mySym); // it gives the value of entity mySum but the type of that entity will string not a symbol, that's why we use above syntax also
-console.log(typeof myObj.mySym); //string  (if entity in myObj is 'mySym' not '[mySym]')
+// console.log(myObj.mySym); // undefined // it gives the value of entity mySum but the type of that entity will string not a symbol, that's why we use above syntax also
+// console.log(typeof myObj.mySym); //undefined //string  (if entity in myObj is 'mySym' not '[mySym]')
 console.log(myObj[mySym]) // key1
 console.log(typeof myObj[mySym]) // string the value stored in mySum
 
@@ -23,7 +23,7 @@ console.log(typeof myObj[mySym]) // string the value stored in mySum
 
 myObj.email = "swapnil@google.com"
 console.log(myObj.email);
-Object.freeze(myObj)
+// Object.freeze(myObj)
 myObj.email = "swapnil@microsoft.com"
 console.log(myObj) // After freezing changes will not reflect in original object
 
@@ -34,8 +34,8 @@ myObj.greeting = function()
 
 myObj.greetingTwo = function()
 {
-    console.log(`hello coder, ${myObj.name}`)
+    console.log(`hello coder, ${this.name}`)
 }
 
-console.log(myObj.greeting());
-console.log(myObj.greetingTwo());
+myObj.greeting();
+myObj.greetingTwo();
